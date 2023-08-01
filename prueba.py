824266f4f688b5e67f3ec4a91f4565753cba53c6
Lucasal2000 <73763025+Lucasal2000@users.noreply.github.com>
2023-08-01 12:49:22 +0200

    Avance1: Cabeza de serpiente
    He añadido comentarios y nueva idea para resolver el problema de la barra. Se trata de usar la esquina derecha de la barra como la cabeza que se mueve. A parte, he añadido los respectivos giros de horizontal a vertical.

--- prueba.py
+++ prueba.py
@@ -1,26 +1,33 @@
-from collections import deque
-def solvelabyrinth(labyrinth):
+from collections import deque #colas necesarias para la resolución
+#definimos la función para resolver el problema
+def solvelabyrinth(labyrinth): 
     n = len(labyrinth)
     m = len(labyrinth[0])
-    start=(0,0)
+    start=(2,0) #empieza en x=2 ya que es la cabeza de la barra 
     queue=deque()
-    queue.appendleft((start[0],start[1],0))
-    directions=[[0,1],[0,-1],[1,0],[-1,0]]
-    visited=[[False]*m for _ in range(n)] 
+    queue.appendleft((start[0],start[1],0)) #introducimos la primera coordenada a la cola
+    directions=[[0,1],[0,-1],[1,0],[-1,0],[-1,1],[1,-1]] #las dos últimas son las rotaciones
+    visited=[[False]*m for _ in range(n)] #matriz mxn rellena de Falses
 
+#Bucle para buscar la salida
     while len(queue) !=0:
         coord=queue.pop()
-        visited[coord[0]][coord[1]]=True
-
+        visited[coord[0]][coord[1]]=True #registramos los lugares visitados
+        #coordenadas coinciden con el final
         if coord[0]==n-1 and coord[1]==m-1:
-            return coord[2]+1
+            return coord[2]
+        #busqueda de nuevas coordenadas
         for dir in directions:
             n1=coord[0]+dir[0]
             m1=coord[1]+dir[1]
+            #no posibles lugares
             if(n1<0 or n1 >=n or m1<0 or m1>=m or labyrinth[n1][m1]=="#" or visited[n1][m1]):continue
+            #nuevo movimiento
             queue.appendleft((n1,m1,coord[2]+1))
+            
 
 
+#Test 1
 labyrinth = [
     [".",".",".",".",".",".",".",".","."],
     ["#",".",".",".","#",".",".",".","."],
@@ -32,3 +39,38 @@ labyrinth = [
 solve = solvelabyrinth(labyrinth)
 print(solve)  
 
+#Test 2
+labyrinth = [
+    [".",".",".",".",".",".",".",".","."], 
+    ["#",".",".",".","#",".",".","#","."], 
+    [".",".",".",".","#",".",".",".","."], 
+    [".","#",".",".",".",".",".","#","."], 
+    [".","#",".",".",".",".",".","#","."]
+]
+solve = solvelabyrinth(labyrinth)
+print(solve)
+
+#Test 3
+labyrinth = [
+    [".",".","."],
+    [".",".","."],
+    [".",".","."]
+]
+solve = solvelabyrinth(labyrinth)
+print(solve)  
+
+#Test 4
+labyrinth = [
+    [".",".",".",".",".",".",".",".",".","."],
+    [".","#",".",".",".",".","#",".",".","."],
+    [".","#",".",".",".",".",".",".",".","."],
+    [".",".",".",".",".",".",".",".",".","."],
+    [".",".",".",".",".",".",".",".",".","."],
+    [".","#",".",".",".",".",".",".",".","."],
+    [".","#",".",".",".","#",".",".",".","."],
+    [".",".",".",".",".",".","#",".",".","."],
+    [".",".",".",".",".",".",".",".",".","."],
+    [".",".",".",".",".",".",".",".",".","."]
+]
+solve = solvelabyrinth(labyrinth)
+print(solve) 
