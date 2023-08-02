804909f08b40c395a718c2467e516c3f522fb9ce
Lucasal2000 <73763025+Lucasal2000@users.noreply.github.com>
2023-08-02 20:51:43 +0200

    Avance6: Final
    El código funciona correctamente y contiene sus respectivas anotaciones

--- prueba.py
+++ prueba.py
@@ -1,5 +1,4 @@
 from collections import deque #colas necesarias para la resolución
-import pdb
 #definimos la función para resolver el problema
 def solvelabyrinth(labyrinth): 
     n = len(labyrinth)
@@ -7,17 +6,14 @@ def solvelabyrinth(labyrinth):
     vertical = False #variable vertical 
     start=(0,2) #empieza en x=2 ya que es la cabeza de la barra 
     queue1=[]
-    queue2=[]
+    queue2=[] #cola para eliminar ubicaciones duplicadas
     queue1.append((start[0],start[1],0,vertical)) #introducimos la primera coordenada a la cola
     queue2.append((start[0],start[1],vertical)) #introducimos la primera coordenada a la cola
     directions=[[0,1],[0,-1],[1,0],[-1,0],[-1,1],[1,-1]] #las dos últimas son las rotaciones
-    #visited=[[False]*m for _ in range(n)] #matriz mxn rellena de Falses
 
 #Bucle para buscar la salida
     while len(queue1) !=0:
-        #pdb.set_trace()
         coord=queue1.pop(0)
-        #visited[coord[0]][coord[1]]=True #registramos los lugares visitados
         #coordenadas coinciden con el final
         if coord[0]==n-1 and coord[1]==m-1:
             return coord[2] 
@@ -30,20 +26,21 @@ def solvelabyrinth(labyrinth):
             vertical = coord[3]
             if(dir==[1,-1] or dir==[1,-1]):
                 vertical = not vertical
-            #if(dir==[-1,1]):
-            #    if(labyrinth[n1 + 1][m]=="#" or labyrinth[n1 - 1][m]=="#" or labyrinth[n1 - 1][m - 2]=="#" or labyrinth[n1 + 1][m -2]=="#"):continue
-            #if(dir==[1,-1]):
-            #    if(labyrinth[n1][m + 1]=="#" or labyrinth[n1][m - 1]=="#" or labyrinth[n1 - 2][m - 1]=="#" or labyrinth[n1 - 2][m + 1]=="#"):continue
+             #probamos que puede rotar en el cuadrado 3x3
+            if(dir==[-1,1]):
+                if(labyrinth[n1 - 1][m1] == "#" or labyrinth[n1 + 1][m1] == "#" or labyrinth[n1 + 1][m1 - 2] == "#" or labyrinth[n1 - 1][m1 - 2] == "#"):continue
+            if(dir==[1,-1]):
+                if(labyrinth[n1][m1 - 1] == "#" or labyrinth[n1][m1 + 1] == "#" or labyrinth[n1 - 2][m1 + 1] == "#" or labyrinth[n1 - 2][m1 - 1] == "#"):continue
+            #comprobar si la cola cumple ras restricciones
             if(vertical == False):
                 if(n1 <0 or n1 >=n or m1 - 1 <0 or m1 - 1 >=m or labyrinth[n1][m1 - 1] == "#"):continue
                 if(n1 <0 or n1 >=n or m1 - 2 <0 or m1 - 2 >=m or labyrinth[n1][m1 - 2] == "#"):continue
-           
             else:
                 if(n1 - 1 <0 or n1 - 1 >=n or m1<0 or m1>=m or labyrinth[n1 - 1][m1]=="#"):continue
                 if(n1 - 2 <0 or n1 - 2 >=n or m1<0 or m1>=m or labyrinth[n1 - 2][m1]=="#"):continue
             #nuevo movimiento
             new_node = (n1,m1,coord[2]+1, vertical)
-            
+            #en caso de que el nievo movimiento no este registrado lo añade
             if (not queue2.count((n1,m1,vertical))):
                 queue1.append(new_node)
                 queue2.append((n1,m1,vertical))    
